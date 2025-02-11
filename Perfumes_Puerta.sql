bbaaf45fc294c7bdcfab049704d385f5c1994131
DalilaDoor <ppuuppyy83@gmail.com>
2025-02-11 03:41:51 -0300

    Update Perfumes_Puerta.sql

--- Perfumes_Puerta.sql
+++ Perfumes_Puerta.sql
@@ -362,26 +362,14 @@ DELIMITER ;
 -- ______________________________________________________________________________________________________
 -- ___________________ TRIGGERS _________________________________________________________________________
 
--- __ Primer trigger: ACTUALIZAR STOCK DESPUES DE VENTA
--- Reduce automáticamente el stock de un producto cuando se registra una venta.
-
+-- __ Primer trigger: before_insert_detalle_venta
+-- Evita ventas con stock insuficiente. Ejemplo
+-- SELECT id_producto, stock FROM producto WHERE id_producto = 1;
+-- INSERT INTO detalle_venta (id_venta, id_producto, cantidad_vendida, precio_unitario)
+-- VALUES (1, 1, 60, 120.50);
+-- resultado = Error Code: 1644. No hay suficiente stock para completar la venta
 DELIMITER //
-CREATE TRIGGER actualizar_stock_despues_de_venta
-AFTER INSERT ON detalle_venta
-FOR EACH ROW
-BEGIN
-    UPDATE producto
-    SET stock = stock - NEW.cantidad_vendida
-    WHERE id_producto = NEW.id_producto;
-END //
-DELIMITER ;
-
-
--- __ Segundo trigger: EVITAR VENTA SIN STOCK
--- Evita ventas cuando no hay suficiente stock disponible, protegiendo la integridad del inventario.
-
-DELIMITER //
-CREATE TRIGGER evitar_venta_sin_stock
+CREATE TRIGGER before_insert_detalle_venta
 BEFORE INSERT ON detalle_venta
 FOR EACH ROW
 BEGIN
@@ -392,10 +380,30 @@ BEGIN
     FROM producto
     WHERE id_producto = NEW.id_producto;
 
-    -- Verificar si hay stock suficiente
+    -- Verificar si hay suficiente stock
     IF stock_disponible < NEW.cantidad_vendida THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Error: No hay suficiente stock disponible para la venta';
+        SET MESSAGE_TEXT = 'No hay suficiente stock para completar la venta';
     END IF;
-END //
+END;
+//
+DELIMITER ;
+
+-- __ Segundo trigger: after_insert_detalle_venta
+-- Actualiza el total de la venta después de insertar un detalle de venta. ejemplo
+-- SELECT * FROM venta WHERE id_venta = 1;
+-- INSERT INTO detalle_venta (id_venta, id_producto, cantidad_vendida, precio_unitario)
+-- VALUES (1, 2, 2, 85.75);
+-- SELECT * FROM venta WHERE id_venta = 1;
+DELIMITER //
+CREATE TRIGGER after_insert_detalle_venta
+AFTER INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    -- Actualizar el total de la venta sumando los productos vendidos
+    UPDATE venta
+    SET total_venta = (SELECT SUM(cantidad_vendida * precio_unitario) FROM detalle_venta WHERE id_venta = NEW.id_venta)
+    WHERE id_venta = NEW.id_venta;
+END;
+//
 DELIMITER ;
